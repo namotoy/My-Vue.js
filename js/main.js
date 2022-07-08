88dfbb9b531fee86ed1f637470a157831a7cef42
namotoy <namotopet0615@gmail.com>
2022-07-08 16:56:36 +0900

    v-onイベント設定

--- js/main.js
+++ js/main.js
@@ -3,12 +3,19 @@
 var vm = new Vue({
   el:"#app",
   data: {
+    newItem:'',
     // name: 'namoto'
     todos: [
       'task 1',
       'task 2',
       'task 3'
     ]
+  },
+  methods: {
+    addItem:function(){
+      this.todos.push(this.newItem);
+      this.newItem = '';
+    }
   }
 });
 }
