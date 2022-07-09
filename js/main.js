dabbf99eefa32e38ea45fbab9553f96d2c464d0c
namotoy <namotopet0615@gmail.com>
2022-07-09 15:45:54 +0900

    算出プロパティの設定

--- js/main.js
+++ js/main.js
@@ -4,17 +4,16 @@ var vm = new Vue({
   el:"#app",
   data: {
     newItem:'',
-  //   todos: [{
-  //     title: 'task 1',
-  //     isDone: true
-  //   },{
-  //     title: 'task 2',
-  //     isDone: true
-  //   },{
-  //     title: 'task 3',
-  //     isDone: false
-  // }]
-  todos:[]
+    todos: [{
+      title: 'task 1',
+      isDone: true
+    },{
+      title: 'task 2',
+      isDone: true
+    },{
+      title: 'task 3',
+      isDone: false
+  }]
   },
   methods: {
     addItem:function(){
@@ -29,6 +28,14 @@ var vm = new Vue({
       if (confirm('本当にいいですか?')){
         this.todos.splice(index,1);
       }
+    }
+  },
+  computed: {
+    remaining: function(){
+      var items = this.todos.filter(function(todo){
+        return !todo.isDone;
+      });
+      return items.length;
     }
   }
 });
