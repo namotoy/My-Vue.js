8bd7b4f75adf03677c0c2077ec05787814f534bc
namotoy <namotopet0615@gmail.com>
2022-07-11 22:01:04 +0900

    いいねボタンの実装

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 'use strict'
 {
+
+var likeComponent = Vue.extend({
+  data: function(){
+    return {
+      count :0
+    }
+  },
+  template: '<button @click="countUp">いいね{{ count }}</button>',
+  methods: {
+    countUp: function(){
+      this.count++;
+    }
+  }
+  });
+
 var vm = new Vue({
   el:"#app",
+  
+  components: {
+  'like-component': likeComponent
+  },
+
   data: {
     newItem:'',
     todos: [
